6d7e7fc97d8ddde2784a7accdb52cc55a03899f3
Blakskyben <general@simplexwebsites.com>
2018-06-28 21:29:10 -0400

    Work on col3.

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ const switch2 = ()=>{
     document.querySelector("#tradeHistory").style.display = "block";
     document.querySelector("#orderHistory").style.display = "none";
 }
+
+const chart1 = ()=>{
+    document.querySelector("#priceChart").style.display = "block";
+    document.querySelector("#depthChart").style.display = "none";
+}
+
+const chart2 = ()=>{
+    document.querySelector("#depthChart").style.display = "block";
+    document.querySelector("#priceChart").style.display = "none";
+}
