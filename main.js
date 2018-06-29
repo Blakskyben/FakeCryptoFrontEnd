dd8609009aa81dac102f61683b954e32ae0e58dc
Blakskyben <general@simplexwebsites.com>
2018-06-29 13:57:11 -0400

    Add charts and add switch chart buttons.

--- main.js
+++ main.js
@@ -10,10 +10,91 @@ const switch2 = ()=>{
 
 const chart1 = ()=>{
     document.querySelector("#priceChart").style.display = "block";
-    document.querySelector("#depthChart").style.display = "none";
+    document.querySelector("#depthChart").style.diplay = "none";
+    document.querySelector(".changeId1").setAttribute("id","graph");
+    document.querySelector(".changeId2").setAttribute("id","none");
+    Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function(err, rows){
+
+    function unpack(rows, key) {
+    return rows.map(function(row) {
+        return row[key];
+    });
+    }
+
+    var trace = {
+    x: unpack(rows, 'Date'),
+    close: unpack(rows, 'AAPL.Close'),
+    high: unpack(rows, 'AAPL.High'),
+    low: unpack(rows, 'AAPL.Low'),
+    open: unpack(rows, 'AAPL.Open'),
+
+    // cutomise colors
+    increasing: {line: {color: 'green'}},
+    decreasing: {line: {color: 'red'}},
+
+    type: 'candlestick',
+    xaxis: 'x',
+    yaxis: 'y'
+    };
+
+    var data = [trace];
+
+    var layout = {
+    dragmode: 'zoom',
+    showlegend: false,
+    xaxis: {
+        rangeslider: {
+            visible: false
+        }
+    }
+    };
+
+    Plotly.plot('graph', data, layout);
+    });
 }
 
 const chart2 = ()=>{
     document.querySelector("#depthChart").style.display = "block";
     document.querySelector("#priceChart").style.display = "none";
+    document.querySelector(".changeId1").setAttribute("id","none");
+    document.querySelector(".changeId2").setAttribute("id","graph");
+    Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function(err, rows){
+
+    function unpack(rows, key) {
+    return rows.map(function(row) {
+        return row[key];
+    });
+    }
+
+    var trace = {
+    x: unpack(rows, 'Date'),
+    close: unpack(rows, 'AAPL.Close'),
+    high: unpack(rows, 'AAPL.High'),
+    low: unpack(rows, 'AAPL.Low'),
+    open: unpack(rows, 'AAPL.Open'),
+
+    // cutomise colors
+    increasing: {line: {color: 'green'}},
+    decreasing: {line: {color: 'red'}},
+
+    type: 'candlestick',
+    xaxis: 'x',
+    yaxis: 'y'
+    };
+
+    var data = [trace];
+
+    var layout = {
+    dragmode: 'zoom',
+    showlegend: false,
+    xaxis: {
+        rangeslider: {
+            visible: false
+        }
+    }
+    };
+
+    Plotly.plot('graph', data, layout);
+    });
 }
+
