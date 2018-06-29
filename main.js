b58efb16f71e548135766f6d0b1ef14624fc3a8e
Blakskyben <general@simplexwebsites.com>
2018-06-28 20:28:34 -0400

    Finish all but col3.

---
+++ main.js
@@ -0,0 +1,9 @@
+const switch1 = ()=>{
+    document.querySelector("#orderHistory").style.display = "block";
+    document.querySelector("#tradeHistory").style.display = "none";
+}
+
+const switch2 = ()=>{
+    document.querySelector("#tradeHistory").style.display = "block";
+    document.querySelector("#orderHistory").style.display = "none";
+}
