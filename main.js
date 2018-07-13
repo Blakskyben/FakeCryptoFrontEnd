e1c227bba34f5a2e3b1c224272484b2607a03969
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-07-13 16:10:24 -0400

    Added local user persistency

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-const switch1 = ()=>{
+const switch1 = () => {
     document.querySelector("#orderHistory").style.display = "block";
     document.querySelector("#tradeHistory").style.display = "none";
-}
+};
 
-const switch2 = ()=>{
+const switch2 = () => {
     document.querySelector("#tradeHistory").style.display = "block";
     document.querySelector("#orderHistory").style.display = "none";
-}
+};
 
-const chart1 = ()=>{
+const chart1 = () => {
     document.querySelector("#ltcChart").style.display = "none";
     document.querySelector("#btcChart").style.display = "block";
     document.querySelector("#ethChart").style.display = "none";
-}
+};
 
-const chart2 = ()=>{
+const chart2 = () => {
     document.querySelector("#ethChart").style.display = "block";
     document.querySelector("#ltcChart").style.display = "none";
     document.querySelector("#btcChart").style.display = "none";
-}
+};
 
-const chart3 = ()=>{
+const chart3 = () => {
     document.querySelector("#ltcChart").style.display = "block";
     document.querySelector("#ethChart").style.display = "none";
     document.querySelector("#btcChart").style.display = "none";
-}
+};
 
+const logIn = (username, email, password) => {
+    localStorage.setItem('username', username);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+};
+
+const logOut = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+};
